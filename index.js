901936c0c68074f9cf9a740f40eb0db592edf2cc
imadghali01 <imadghali01@gmail.com>
2025-02-04 09:42:29 +0100

    remove-node-module/git-ignore

--- index.js
+++ index.js
@@ -30,6 +30,31 @@ app.get("/api/test", (req, res) => {
     res.json(user);
   });
 });
+app.get("/api/test", (req, res) => {
+  // Requête pour récupérer l'utilisateur ayant l'ID = 1
+  const id = req[0];
+  const mdp = req[1];
+  const query = "SELECT Mdp AND Activity FROM users WHERE Ncontract = req[0]";
+  if (query == mdp) {
+  }
+  db.query(query, (err, results) => {
+    if (err) {
+      console.error("Erreur lors de la requête :", err);
+      return res.status(500).send("Erreur serveur");
+    }
+
+    // Vérification si au moins un enregistrement est trouvé
+    if (results.length === 0) {
+      return res
+        .status(404)
+        .json({ message: "Aucun utilisateur trouvé avec cet ID" });
+    }
+
+    // Retourner le premier résultat (user avec Id=1) sous forme d'objet JSON
+    const user = results[0];
+    res.json(user);
+  });
+});
 
 app.listen(port, () => {
   console.log("server online");
