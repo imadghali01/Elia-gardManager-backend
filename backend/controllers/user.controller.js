d3685f2a131f30a01ae65a3fb557e24ebbe49969
jeffrey <jeffreydevillecode@gmail.com>
2025-02-19 10:05:36 +0100

    user id et activity

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -85,7 +85,9 @@ module.exports = {
 
       return res.status(200).json({
         message: "Connexion réussie", // ça informe à le frontend que la connecion est réussie.
-        userId: req.session.userId, // --> Ici ça retourne l'ID stocké en session, pour faire la vérification côté frontend.
+        userId: user._id,
+        role:   user.activity,
+        // --> Ici ça retourne l'ID stocké en session, pour faire la vérification côté frontend.
       });
     } catch (error) {
       return res.status(500).json({ error: error.message });
